5eaf84a0d5fdab294ed92f463591fb0584b98a67
Jay Kang <yjk441@nyu.edu>
2021-03-21 00:42:11 -0400

    final push?

--- Week6/script.js
+++ Week6/script.js
@@ -1,9 +1,47 @@
+class Dialogue {
+    constructor(length, text_arr, pic_arr) {
+        this.length = length;
+        this.text_arr = text_arr;
+        this.pic_arr = pic_arr;
+    }
+}
+
+function advance_text(dialogue, dia_num) {
+    if (dia_num[0] >= dialogue.length) {
+        return;
+    }
+    else {
+        document.getElementById("description").innerHTML = dialogue.text_arr[dia_num[0]];
+        document.getElementById("screen_pic").src = dialogue.pic_arr[dia_num[0]];
+        dia_num[0] += 1;
+    }
+}
+
+function change_topic(dialogue) {
+    current_dialogue = dialogue;
+    dia_num[0] = 0;
+    advance_text(current_dialogue, dia_num)
+}
+
+
 function change_text() {
-    document.getElementById("description").innerHTML = "The fire lights up the room, but only dimly.";
+    document.getElementById("description").innerHTML = "The fire lights up the room, but only dimly. (1/1)";
 }
 
 function change_text2() {
     document.getElementById("description").innerHTML = "You talk to yourself. Your party members look at you strangely.";
+}
+
+function update_tooltip(action) {
+    var tooltip = document.getElementById("tooltip_desc")
+    tooltip.innerHTML = action;
+    if (tooltip.style.opacity == 0) {
+        tooltip.style.opacity = 1;
+    }
+    else {
+        tooltip.style.opacity = 0;
+    }
+    
 }
 
 function change_page_p1() {
